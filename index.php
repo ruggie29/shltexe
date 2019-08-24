ec2641c09052f30ba14e1d1e2b10cabbbfb22b19
ruggie29 <54447787+ruggie29@users.noreply.github.com>
2019-08-24 17:35:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+
+include "function.php";
+
 //è necessario aggiungere bot prima del nostro token
 //You must add bot before our token
 $botToken = "bot"."969847936:AAEHT-nNJsOTRSyqhJeazDm3TCh1ZaBd1XU";
 
-
-include "function.php";
 
 //Recuperiamo l'input che riceveremo dal bot
 //We retrieve the input we receive from bot
