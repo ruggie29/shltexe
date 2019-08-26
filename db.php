60b6683ea02c9289d5be2012bfadbd39bc3c5283
ruggie29 <54447787+ruggie29@users.noreply.github.com>
2019-08-26 12:53:59 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,12 @@
 <?php
-mysql_connect('ftp.rug.altervista.org','rug','2ZbmqKxaxpsm');
-mysql_select_db('my_rug')
-
+//mysql_connect('ftp.rug.altervista.org','rug','2ZbmqKxaxpsm');
+//mysql_select_db('my_rug')
+$host = "localhost";
+$username = "rug";
+$password = "2ZbmqKxaxpsm";
+$database = "my_rug";
+  
+$db = mysql_connect($host, $username, $password) or die("Errore durante la connessione al database");
+mysql_select_db($database, $db) or die("Errore durante la selezione del database"); 
 
 ?>
