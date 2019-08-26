22f7bae7961e87e6535d2d7b2f18dfd2460e321f
ruggie29 <54447787+ruggie29@users.noreply.github.com>
2019-08-26 12:31:39 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-mysql_connect('https://s428.altervista.org/phpmyadmin/?sid=8636c3808fe4c6fb28a6c0f3d6c484c6#PMAURL-8:index.php?db=&table=&server=1&target=&token=22c64a5e61303e736687db3d1c4d59d7','rug','2ZbmqKxaxpsm');
+mysql_connect('localhost','rug','2ZbmqKxaxpsm');
 mysql_select_db('my_rug')
 
 
