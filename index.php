870bf25faf998224da20aadb71abef90388c0f8d
ruggie29 <54447787+ruggie29@users.noreply.github.com>
2019-09-06 15:30:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
-<?php
-
-
-//$botToken = "bot"."969847936:AAEHT-nNJsOTRSyqhJeazDm3TCh1ZaBd1XU";
-
-//è necessario aggiungere bot prima del nostro token
-//You must add bot before our token
 include "function.php";
 include "bandit.php";
 include "bot.php";
-//Recuperiamo l'input che riceveremo dal bot
-//We retrieve the input we receive from bot
+
 $TelegramRawInput = file_get_contents("php://input");
-// php://input restituisce i dati raw (testo), andremo quindi a formattare il tutto in un array, i dati che riceveremo saranno in formato Json quindi un json_decode farà al caso nostro.
-// php // input returns the raw data (text), then we will format everything in an array, the data we receive will be in Json format so a json_decode will be for us.
+
 $update = json_decode($TelegramRawInput, TRUE);
 
-//Assicuriamoci di aver ricevuto un update, altrimenti interrompiamo l'esecuzione
-//Make sure you have received an update, otherwise we interrupt execute
 if(!$update)
 {
   //exit;
 }
 
-//Recuperiamo l'oggetto message dal json
-//We recover the message object from json
 $MessageObj = $update['message'];
-//Recuperiamo il chatId, che utilizzeremo per rispondere all'utente che ci ha appena invocato
-//We recover the chatId table which we will use to respond to the user who has just invoked
 $chatId = $MessageObj['chat']['id'];
 $MikeId = "424842427";
 $testo = $MessageObj['text'];
@@ -41,15 +26,3 @@ case 'Fungi':
      $out = sendMsg($botToken,$chatId,"Fungo");
     break;
 }
-
-//Rispondiamo HelloWorld
-//We answer HelloWorld
-//$out = sendMsg($botToken,$chatId,"Invio un messaggio!");
-
-//Salvo il json ricevuto per analizzarlo in seguito
-//We save the json received to parse it later
-//saveInJsonFile($out, "inviato.json");
-
-
-
-?>
