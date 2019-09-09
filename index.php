fa8713efda17d8a448f0c138f02167e0e729fe35
ruggie29 <54447787+ruggie29@users.noreply.github.com>
2019-09-09 20:07:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if($querydata == "povero")
   //$out = editMsg($botToken,$queryuser,$querymsg,"Povero");
  // $inline2 = '[{"text" : "<<Indietro", "callback_data" : "back"},{"text" : "Dove Pullare", "callback_data" : "povero", "show_allert" : true}]';
  // $out = editMsg($botToken,$queryuser,$querymsg,"Stai fermo che sei povero",$inline2,"inline");
- $out= getAllert ($botToken,$queryuser,"Povero");
+ $out= getAllert ($botToken,$querychat,"Povero");
   
 }
 
