11de2908fb13619333eb6f9c2acb09c230a19112
ruggie29 <54447787+ruggie29@users.noreply.github.com>
2019-09-09 15:49:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ break;
   case '/info':
     $info = getmembers($botToken,'-1001469403482','773826549');
     $finale = json_decode($info, TRUE);
-    $result =$finale ['result']['status'];
+    $result =$finale['result']['status'];
     $out = sendMsg($botToken,$chatId,"$result");
   break;
     
