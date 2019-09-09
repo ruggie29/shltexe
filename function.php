16d0ebd6521f6a84e23e4ff22010ca5fa2e6acb8
ruggie29 <54447787+ruggie29@users.noreply.github.com>
2019-09-09 12:13:21 +0200

    Update function.php

--- function.php
+++ function.php
@@ -56,7 +56,7 @@ function countmembers ($tkn,$cId)
 
     $int=file_get_contents($TelegramUrlSendMessage);
     
-    $result = $int['ok']['result'];
+    $result = $int['message']['result'];
     
     return $result;
 }
