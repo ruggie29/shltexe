bae3a2523ab252361091880e8bca303af010e49f
ruggie29 <54447787+ruggie29@users.noreply.github.com>
2019-09-09 20:56:51 +0200

    Update function.php

--- function.php
+++ function.php
@@ -87,7 +87,7 @@ function getAllert ($tkn,$queryId,$msgTxt)
 
 function replayMsg ($tkn,$cId,$msgId, $msgTxt)
 {
-    $TelegramUrlSendMessage = "https://api.telegram.org/".$tkn."//sendMessage?chat_id=".$cId."&parse_mode=HTML&text=".urlencode($msgTxt)."&reply_to_message_id=".$msgId.;
+    $TelegramUrlSendMessage = "https://api.telegram.org/".$tkn."//sendMessage?chat_id=".$cId."&parse_mode=HTML&text=".urlencode($msgTxt)."&reply_to_message_id=".$msgId;
     
     return file_get_contents($TelegramUrlSendMessage);
 }
